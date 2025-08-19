683fa4f83807e5e2f553a1054da8c84e147823ec
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-19 12:14:28 -0400

    Primer commit

---
+++ src/main/java/com/example/postmylistdoubled/PostMyListDoubledApplication.java
@@ -0,0 +1,13 @@
+package com.example.postmylistdoubled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMyListDoubledApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PostMyListDoubledApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/postmylistdoubled/PostMyListDoubledApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postmylistdoubled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostMyListDoubledApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
